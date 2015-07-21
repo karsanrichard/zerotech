c40e4f4632c5ed0c7b67cfe138d5d7881e70be88
bakasajoshua <baksajoshua09@gmail.com>
2015-07-21 12:03:35 +0300

    htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost:81/zerocorp/';
+$config['base_url'] = 'http://localhost/zerotech/';
 
 /*
 |--------------------------------------------------------------------------
