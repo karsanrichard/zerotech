3ec8a6a0c22847614f2fda8870505a25181f3694
Richard Karsan <karsanrichard@gmail.com>
2015-07-31 17:55:35 +0300

    man kuna baridi

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'homestead';
-$db['default']['password'] = 'secret';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'zerocorp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
