289443c16a78f4318c27c11e89328dea1281ce75
Richard Karsan <karsanrichard@gmail.com>
2015-08-30 20:28:27 +0300

    Prior to Josh pull

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('ASSETS_URL', 'http://zerocorp.app:8000/assets/');
+define('ASSETS_URL', 'http://localhost/zerocorp/assets/');
 include_once './vendor/autoload.php';
 /*
  *---------------------------------------------------------------
